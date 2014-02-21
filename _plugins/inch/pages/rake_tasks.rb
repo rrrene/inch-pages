b776fcde69e8e37d4e29c46182e32de6c0ac8fda
René Föhring <rf@bamaru.de>
2014-02-21 19:55:46 +0100

    Clean up; new logo; release works

--- _plugins/inch/pages/rake_tasks.rb
+++ _plugins/inch/pages/rake_tasks.rb
@@ -19,6 +19,7 @@ module Inch
 
       def initialize(base = nil)
         @base = (base ||= Dir.pwd)
+        @inch_pages_dir = File.join(base, "..", "inch-pages.github.io")
       end
 
       def install
@@ -37,62 +38,110 @@ module Inch
 
       def build_site
         sh("jekyll build")
+        copy_site
         success "Built site."
       end
 
       def release_site
-        guard_clean
+        guard_clean(@inch_pages_dir)
         build_site
-        git_push
+        git_add_all(@inch_pages_dir)
+        git_commit_all(commit_msg, @inch_pages_dir)
+        git_push(@inch_pages_dir)
       end
 
       protected
 
-      def git_push
-        perform_git_push
+      def commit_msg
+        uid = Time.now.strftime("%Y-%m-%d %H:%M")
+        "Publish inch-pages #{uid}"
+      end
+
+      def copy_site(target = @inch_pages_dir)
+        glob = File.join(base, "_site", "*")
+        Dir[glob].each do |f|
+          FileUtils.cp_r f, target
+        end
+      end
+
+      def git_push(dir)
+        perform_git_push(dir)
         success "Pushed to GitHub."
       end
 
-      def perform_git_push(options = '')
+      def perform_git_push(dir, options = '')
         cmd = "git push #{options}"
-        out, code = sh_with_code(cmd)
-        raise "Couldn't git push. `#{cmd}' failed with the following output:\n\n#{out}\n" unless code == 0
+        out, code = sh_with_code(cmd, dir)
+        fail "Couldn't git push. `#{cmd}' failed with the following output:\n\n#{out}\n" unless code == 0
       end
 
-      def guard_clean
-        clean? && committed? or raise("There are files that need to be committed first.")
+      def git_add_all(dir)
+        cmd = "git add ."
+        out, code = sh_with_code(cmd, dir)
+        fail "Couldn't git add. `#{cmd}' failed with the following output:\n\n#{out}\n" unless code == 0
       end
 
-      def clean?
-        sh_with_code("git diff --exit-code")[1] == 0
+      def git_commit_all(msg, dir)
+        cmd = "git commit -m \"#{msg}\""
+        out, code = sh_with_code(cmd, dir)
+        if code != 0
+          if out =~ /nothing to commit/
+            abort "nothing new to publish."
+          else
+            fail "Couldn't git commit. `#{cmd}' failed with the following output:\n\n#{out}\n"
+          end
+        end
       end
 
-      def committed?
-        sh_with_code("git diff-index --quiet --cached HEAD")[1] == 0
+      def guard_clean(dir = base)
+        clean?(dir) && committed?(dir) or fail("There are files in #{dir} that need to be committed first.")
       end
 
-      def sh(cmd, &block)
-        out, code = sh_with_code(cmd, &block)
-        code == 0 ? out : raise(out.empty? ? "Running `#{cmd}' failed. Run this command directly for more detailed output." : out)
+      def clean?(dir)
+        sh_with_code("git diff --exit-code", dir)[1] == 0
       end
 
-      def sh_with_code(cmd, &block)
+      def committed?(dir)
+        sh_with_code("git diff-index --quiet --cached HEAD", dir)[1] == 0
+      end
+
+      def sh(cmd, dir = nil, &block)
+        out, code = sh_with_code(cmd, dir, &block)
+        code == 0 ? out : fail(out.empty? ? "Running `#{cmd}' failed. Run this command directly for more detailed output." : out)
+      end
+
+      def sh_with_code(cmd, dir = nil, &block)
+        dir ||= base
         cmd << " 2>&1"
         outbuf = ''
-        old_dir = Dir.pwd
-        Dir.chdir base
-
-        outbuf = `#{cmd}`
-        if $? == 0
-          block.call(outbuf) if block
+        chdir(dir) do
+          outbuf = `#{cmd}`
+          if $? == 0
+            block.call(outbuf) if block
+          end
         end
-
-        Dir.chdir old_dir
         [outbuf, $?]
+      end
+
+      def abort(msg)
+        warn "\u2717 #{msg}".color(:yellow)
+        exit 2
+      end
+
+      def fail(msg)
+        warn "\u2717 #{msg}".color(:red)
+        exit 1
       end
 
       def success(msg)
         puts "\u2713 #{msg}".color(:green)
+      end
+
+      def chdir(dir, &block)
+        old_dir = Dir.pwd
+        Dir.chdir dir
+        yield
+        Dir.chdir old_dir
       end
     end
   end
